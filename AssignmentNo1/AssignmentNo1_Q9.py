f740c615a15a3ddfde8ad5d712d8f532c678666c
ShivamShete0311 <sheteshivam03@gmail.com>
2026-06-15 01:49:10 +0530

    Add files via upload

---
+++ AssignmentNo1/AssignmentNo1_Q9.py
@@ -0,0 +1,10 @@
+#Indentation Error at line no.8
+
+
+print("How many assignments have you completed?")
+a = int(input())
+
+if (a<10):
+print ("You need to study more...")
+else:
+    print ("Good, Keep it up")
